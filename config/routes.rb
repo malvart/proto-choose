db44c60d9d5603a4705a66953d7aedf750454617
malvart <102654106+malvart@users.noreply.github.com>
2022-05-05 18:40:44 +0900

    chooseSearchの調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cookings#index'
-  resources :cookings, only: [:index, :new, :create, :show, :destroy] do
+  resources :cookings do
     collection do
       get 'chooseIndex'
       get 'chooseSearch'
